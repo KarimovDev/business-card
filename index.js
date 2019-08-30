0d287b07c6a61d7eeedca6bc553f29147970a867
dkarimov <dkkarim2@mts.ru>
2019-08-30 11:48:30 +0300

    feat: make layout

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener('scroll', () => {
+    const velocity = 0.2;
+    const position = window.pageYOffset;
+    const element = document.getElementById('parallax-container');
+    
+    element.style.backgroundPosition = `50% ${-Math.round(position * velocity)}px`;
+});
