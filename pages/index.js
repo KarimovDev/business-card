f0f1360c35f24dd85a67a227daa46d163eb8ddbc
KarimovDev <kardimail@gmail.com>
2019-10-28 21:38:05 +0300

    fix: some issues

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const App = () => (
             <meta httpEquiv="X-UA-Compatible" content="ie=edge"></meta>
             <link rel="icon" type="image/png" href="/static/images/favicon.png"></link>
         </Head>
-        <div id="parallax-container">
+        <div className="content-container">
             <div className="content">
                 <div className="content-header">
                     <div className="header-image-wrapper">
